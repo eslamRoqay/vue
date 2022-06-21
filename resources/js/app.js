d500c398c6571f166c651aeb8c10d7cb51d12677
eslamRoqay <102512020+eslamRoqay@users.noreply.github.com>
2022-06-21 11:10:19 +0200

    product price validation front

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,21 @@
 import './bootstrap';
 import {createApp} from "vue";
+import {createRouter , createWebHistory} from "vue-router";
 import PostIndex from './components/posts/index'
+import PostCreate from './components/posts/create'
 import LaravelVuePagination from 'laravel-vue-pagination';
 
+const routes=[
+    {path: '/' , component:PostIndex },
+    {path: '/posts/create' , component:PostCreate }
+]
+const router =createRouter({
+    history:createWebHistory(),
+    routes
+})
+
+
 const app=createApp({})
-app.component('posts-index',PostIndex)
 app.component(  'Pagination', LaravelVuePagination)
+app.use(router)
 app.mount('#app')
